465d02cb77b6f07d545f58ea65c8d07ea459ccdd
Ryan Yoo <ryan.yoo@avanade.com>
2018-12-10 12:52:30 -0600

    Completed OWIN and Katana - Middleware

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace KatanaIntro
 {
@@ -39,7 +40,40 @@ namespace KatanaIntro
             //});
 
             //app.Use<HelloWorldComponent>();
+
+            //app.Use(async (environment, next) =>
+            //{
+            //    foreach (var pair in environment.Environment)
+            //    {
+            //        Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+            //    }
+
+            //    await next();
+            //});
+
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("Requesting : " + environment.Request.Path);
+
+                await next();
+
+                Console.WriteLine("Response: " + environment.Response.StatusCode);
+            });
+
+            ConfigureWebApi(app);
+
             app.UseHelloWorld();
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultApi", 
+                "api/{controller}/{id}", 
+                new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
         }
     }
 
